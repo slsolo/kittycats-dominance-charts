d5a78bd66c5a99d334d25723da291f497c4ec97c
Rob Spencer <rob@robspencer.me.uk>
2023-02-14 20:11:39 +0000

    feat(src): switch to react

---
+++ src/features/counter/counterAPI.js
@@ -0,0 +1,5 @@
+// A mock function to mimic making an async request for data
+export function fetchCount() {
+  return fetch("https://content-sheets.googleapis.com/v4/spreadsheets/10-9WtItK0LWyUSZqnI_6sGngJXtgVsSaSYJd2GN3qqw?ranges=Fur!B2:B&ranges=Eyes!B2:B&includeGridData=true&key=AIzaSyBqNvvKK-xrp1C1PyUuN8tdL5AiOaE4Q0M"
+)
+}
