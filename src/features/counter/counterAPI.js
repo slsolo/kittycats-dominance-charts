59fde5b31befe0616b96fa058219e6c8753f2587
Rob Spencer <rob@robspencer.me.uk>
2023-02-14 22:08:30 +0000

    obscure secrets

--- src/features/counter/counterAPI.js
+++ src/features/counter/counterAPI.js
@@ -1,5 +1,4 @@
-// A mock function to mimic making an async request for data
-export function fetchCount() {
-  return fetch("https://content-sheets.googleapis.com/v4/spreadsheets/10-9WtItK0LWyUSZqnI_6sGngJXtgVsSaSYJd2GN3qqw?ranges=Fur!B2:B&ranges=Eyes!B2:B&includeGridData=true&key=AIzaSyBqNvvKK-xrp1C1PyUuN8tdL5AiOaE4Q0M"
+export function fetchSpreadsheetData() {
+  return fetch(`https://content-sheets.googleapis.com/v4/spreadsheets/${process.env.REACT_APP_SPREADSHEET_ID}?ranges=Fur!B2:B&ranges=Eyes!B2:B&includeGridData=true&key=${process.env.REACT_APP_AUTH_KEY}`
 )
 }
