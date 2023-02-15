d80dc18c3e249fe23a21199f5b0192cc25bba48b
Rob Spencer <rob@robspencer.me.uk>
2023-02-15 02:16:30 +0000

    add shades, tails and ears

--- src/features/counter/counterAPI.js
+++ src/features/counter/counterAPI.js
@@ -1,4 +1,4 @@
 export function fetchSpreadsheetData() {
-  return fetch(`https://content-sheets.googleapis.com/v4/spreadsheets/${process.env.REACT_APP_SPREADSHEET_ID}?ranges=Fur!B2:B&ranges=Eyes!B2:B&includeGridData=true&key=${process.env.REACT_APP_AUTH_KEY}`
+  return fetch(`https://content-sheets.googleapis.com/v4/spreadsheets/${process.env.REACT_APP_SPREADSHEET_ID}?ranges=Fur!B2:B&ranges=Eyes!B2:B&ranges=Tails!A2:A&ranges=Ears!A2:A&ranges=Other!F3:F&includeGridData=true&key=${process.env.REACT_APP_AUTH_KEY}`
 )
 }
